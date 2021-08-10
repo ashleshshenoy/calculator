d6ebcc5c2778e15d11c7d4d5cdd8bf8a73796628
ashleshshenoy <ashleshshenoy09@gmail.com>
2021-08-10 12:04:01 +0530

    last commit

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
+var res = document.getElementById("result");
+var pre = document.getElementById('previous-result');
+
+
+
 
 function clearAll() {
 
-  document.getElementById("result").innerHTML="";
-  document.getElementById("previous-result").innerHTML="";
+  res.innerHTML="";
+  pre.innerHTML="";
 }
+
+
 
 function deleteElement(){
-   var e=document.getElementById("result").innerHTML
-   document.getElementById('result').innerHTML=e.slice(0,e.length-1)
+
+   document.getElementById('result').innerHTML=res.innerHTML.slice(0,res.innerHTML.length-1)
+   demagnify();
+   getAnswer();
 
 }
+
+
 
 function putElement(value){
-  var e=document.getElementById('result').innerHTML
-  document.getElementById('result').innerHTML =  e+String(value)
+
+  if(res.innerHTML){
+  if( res.innerHTML.length > 15){
+    alert(' character limit reached');
+    return
+  }
+}
+  document.getElementById('result').innerHTML =  res.innerHTML+String(value)
+
+  demagnify();
+  getAnswer();
+
+}
+
+function demagnify(){
+  res.style.fontSize = "2rem"
+  pre.style.fontSize = "1.2rem"
+  res.style.opacity =" 1";
+  pre.style.opacity= "0.7";
 }
 
 
+function magnify(){
+   pre.innerHTML = '= ' + pre.innerHTML
+   pre.style.fontSize = "1.6rem"
+   res.style.fontSize = "1.2rem"
+   res.style.opacity =" 0.7";
+   pre.style.opacity= "1";
+
+}
 
 function getAnswer(){
-  var e = document.getElementById("result").innerHTML;
-  var ans=0;
-  while(/[/ *]/.test(e))
-  {
-      var pos = e.search(/[/ *]/d);
-      if( e.match(/[/ *]/) =="/" ){
-            ans= Number(e[pos-1])/ Number(e[pos+1]);
-            e = e.slice( 0,pos-1) + ans + e.slice(pos+2,e.length);
-            }
-        else{
-          ans= Number(e[pos-1])*Number(e[pos+1]);
-          e = e.slice( 0,pos-1) + ans + e.slice(pos+2,e.length);
-        }
-
-  }
-
-  while(/[+ - ]/.test(e))
-  {
-      var pos = e.search(/[+ - ]/d);
-      if( e.match(/[+ - ]/) =="+" ){
-            ans= Number(e[pos-1])+Number(e[pos+1]);
-            e = e.slice( 0,pos-1) + ans + e.slice(pos+2,e.length);
-            }
-        else{
-          ans= Number(e[pos-1]) - Number(e[pos+1]);
-          e = e.slice( 0,pos-1) + String(ans) + e.slice(pos+2,e.length);
-        }
-
-  }
-  document.getElementById('previous-result').innerHTML = e
+   document.getElementById('previous-result').innerHTML = MATH. eval(res.innerHTML)
 }
